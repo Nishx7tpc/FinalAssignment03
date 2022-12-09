489215531f0e7cb0496353e7106827a5759ea2f4
Nishx7tpc <taniscrosschild@gmail.com>
2022-12-08 22:57:44 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,144 @@
+document.title = "BFLookandFind"
+
+var saltsound = document.getElementById("saltsound")
+var milksound = document.getElementById("milksound")
+var eggssound = document.getElementById("eggssound")
+var applesound = document.getElementById("applesound")
+var popsound = document.getElementById("popsound")
+var chickensound = document.getElementById("chickensound")
+var bananasound = document.getElementById("bananasound")
+var ducksound = document.getElementById("ducksound")
+var baconsound = document.getElementById("baconsound")
+var wolfsound = document.getElementById("wolfsound")
+var mousesound = document.getElementById("mousesound")
+var spidersound = document.getElementById("spidersound")
+
+
+
+$("#SALT").on("click", function () {
+    $("#i1").toggle('slow');
+    $(this).remove();
+})
+$("#MILK").on("click", function () {
+    $("#i2").toggle('slow');
+    $(this).remove();
+})
+$("#EGGS").on("click", function () {
+    $("#i3").toggle('slow');
+    $(this).remove();
+})
+$("#APPLE").on("click", function () {
+    $("#i4").toggle('slow');
+    $(this).remove();
+})
+$("#POP").on("click", function () {
+    $("#i5").toggle('slow');
+    $(this).remove();
+})
+$("#CHICKEN").on("click", function () {
+    $("#i6").toggle('slow');
+    $(this).remove();
+})
+$("#BANANA").on("click", function () {
+    $("#i7").toggle('slow');
+    $(this).remove();
+})
+$("#DUCK").on("click", function () {
+    $("#i8").toggle('slow');
+    $(this).remove();
+})
+$("#BACON").on("click", function () {
+    $("#i9").toggle('slow');
+    $(this).remove();
+})
+$("#WOLF").on("click", function () {
+    $("#i10").toggle('slow');
+    $(this).remove();
+})
+$("#MOUSE").on("click", function () {
+    $("#i11").toggle('slow');
+    $(this).remove();
+})
+$("#SPIDER").on("click", function () {
+    $("#i12").toggle('slow');
+    $(this).remove();
+})
+
+$("#SALT").on("click", function () {
+    saltsound.play()
+})
+$("#MILK").on("click", function () {
+    milksound.play()
+})
+$("#EGGS").on("click", function () {
+    eggssound.play()
+})
+$("#APPLE").on("click", function () {
+    applesound.play()
+})
+$("#POP").on("click", function () {
+    popsound.play()
+})
+$("#CHICKEN").on("click", function () {
+    chickensound.play()
+})
+$("#BANANA").on("click", function () {
+    bananasound.play()
+})
+$("#DUCK").on("click", function () {
+    ducksound.play()
+})
+$("#BACON").on("click", function () {
+    baconsound.play()
+})
+$("#WOLF").on("click", function () {
+    wolfsound.play()
+})
+$("#MOUSE").on("click", function () {
+    mousesound.play()
+})
+$("#SPIDER").on("click", function () {
+    spidersound.play()
+})
+$("#i1").on("click", function () {
+    saltsound.play()
+})
+$("#i2").on("click", function () {
+    milksound.play()
+})
+$("#i3").on("click", function () {
+    eggssound.play()
+})
+$("#i4").on("click", function () {
+    applesound.play()
+})
+$("#i5").on("click", function () {
+    popsound.play()
+})
+$("#i6").on("click", function () {
+    chickensound.play()
+})
+$("#i7").on("click", function () {
+    bananasound.play()
+})
+$("#i8").on("click", function () {
+    ducksound.play()
+})
+$("#i9").on("click", function () {
+    baconsound.play()
+})
+$("#i10").on("click", function () {
+    wolfsound.play()
+})
+$("#i11").on("click", function () {
+    mousesound.play()
+})
+$("#i12").on("click", function () {
+    spidersound.play()
+})
+
+
+
+
+
+
